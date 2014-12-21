76667330b3163466e72ec2c25f7c94e36dd2b8c3
Chip Grandits <boulderchip@gmail.com>
2014-12-21 13:12:56 -0700

    implemented based on example for mean, basic interactive testing

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## a function to create a matrix with a cacheable inverse
+## and another function to make use that cache feature
 
-## Write a short comment describing this function
+## creates a special matrix object that is able to cache it's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cachedinv <- NULL
+    
+    set <- function(y) {
+        x <<- y
+        cachedinv <<- NULL
+    }
+    
+    get <- function() x
+    
+    setinverse <- function(inverse) cachedinv <<- inverse
+    
+    getinverse <- function() cachedinv
+    
+    list( set = set, get = get,
+          setinverse = setinverse,
+          getinverse = getinverse )
 }
 
 
-## Write a short comment describing this function
+## gets the inverse of a special matrix object, made from makeCacheMatrix
+## if value is already cached, uses that; if not will solve and 
+## cache value for future use
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    mat <- x$get()
+    i <- solve(mat,...)
+    x$setinverse(i)
+    i
 }
